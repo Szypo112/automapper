5e738bfdf9da63aaab87aa4d0c4ce82598298e67
Szyposzynski, Mateusz <mateusz.szyposzynski@teleste.com>
2020-03-30 17:56:08 +0200

    First example end

---
+++ Automapper.Example/Automapper.Example/Profiles/MovieProfile.cs
@@ -0,0 +1,20 @@
+﻿using Automapper.Example.Model;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automapper.Example.Profiles
+{
+    public class MovieProfile : Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<Movie, MovieDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ExternalId))
+                .ReverseMap()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
--- Automapper.Example/Automapper.Example/Program.cs
+++ Automapper.Example/Automapper.Example/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Automapper.Example.Model;
+using Automapper.Example.Profiles;
+using AutoMapper;
+using System;
 
 namespace Automapper.Example
 {
@@ -6,6 +9,15 @@ namespace Automapper.Example
     {
         static void Main(string[] args)
         {
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new MovieProfile()));
+
+            var mapper = configuration.CreateMapper();
+
+            var movie = new Movie() { ExternalId = new Guid(), Artist = "Art Artis", Title = "Title" };
+
+            var movieDto = mapper.Map<MovieDto>(movie);
+
+            var movieAgain = mapper.Map<Movie>(movieDto);
 
         }
     }
