6cf94d030a01b18a06067863746eac4944c53678
Mateusz Szyposzyski <mateusz.szyposzynski@interia.pl>
2020-03-30 11:10:30 +0200

    basic_start

---
+++ Automapper.Example/Automapper.Example/Model/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automapper.Example.Model
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+        public Guid ExternalId { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+    }
+}
---
+++ Automapper.Example/Automapper.Example/Model/MovieDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automapper.Example.Model
+{
+    public class MovieDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+    }
+}
